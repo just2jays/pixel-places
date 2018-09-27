390a1c6af84778cd45897174d0bd49b41fbe4ac6
JJ Guaragno <jguaragno@mvpub.com>
2018-09-27 18:45:18 -0400

    hood home counting

--- src/Neighborhood.js
+++ src/Neighborhood.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
-import House from './House'
+import House from './House';
+import isEmpty from 'lodash/isEmpty';
 import './App.css';
 
 class Neighborhood extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      homes: []
+    }
+
+    this.hoodElement = React.createRef();
+  }
+
+  componentDidMount() {
+    let homes = [];
+    for (var i = 0; i < 100; i++) {
+      homes.push(
+        <House
+          key={i}
+          neighborhood={this.hoodElement}
+        />
+      );
+    }
+    this.setState({
+      homes: homes
+    });
+  }
+
   generateColor() {
-      // random shade of green (ala grass)
+      // Random shade of green (ala grass)
       var max = 150;
       var min = 100;
       var green = Math.floor(Math.random() * (max - min + 1)) + min;
       return `rgb(0, ${green}, 0)`;
       
-      // random color in the hexadecimal range
+      // Random color in the hexadecimal range
       // return '#' +  Math.random().toString(16).substr(-6);
   }
 
   render() {
+    if(isEmpty(this.state.homes)) return null;
+
     var cardStyle = {
       height: 100,
       width: 100,
@@ -24,14 +51,13 @@ class Neighborhood extends Component {
       cursor: "crosshair"
     };
 
-    var homes = [];
-    for (var i = 0; i < 100; i++) {
-      homes.push(<House key={i} />);
-    }
-
     return(
-      <div style={cardStyle}  className="neighborhood">
-        {homes}
+      <div
+        ref={this.hoodElement}
+        style={cardStyle} 
+        className="neighborhood"
+      >
+        {this.state.homes}
       </div>
     );
   }
